564a37547ee6efcb097ca680b6f28e68e6c0812c
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-06-15 20:10:03 -0300

    first commit

---
+++ src/main/java/br/com/Java17Features/Java17FeaturesApplication.java
@@ -0,0 +1,18 @@
+package br.com.Java17Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17FeaturesApplication.class, args);
+		
+		System.out.println("\n----------------------");
+		System.out.println("ENTROU NA APLICAÇÃO!!!");
+		System.out.println("----------------------\n");
+		
+	}
+
+}
---
+++ src/test/java/br/com/Java17Features/Java17FeaturesApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.Java17Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+		
+	}
+
+}
